c7132292e8c3bc20a7aaa2bebeec688dc1ab7b52
Matt Boubin <mjb9353@nyu.edu>
2026-01-06 18:40:47 -0500

    fixed user pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/boubinmj.github.io/',
+  // User site: hosted at https://boubinmj.github.io/
+  // base defaults to '/', so you can either omit it or write:
+  // base: '/',
 })
