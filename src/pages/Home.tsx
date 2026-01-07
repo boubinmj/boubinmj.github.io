eccf4f568f4c93b02c3018328adac142e516366a
Matt Boubin <mjb9353@nyu.edu>
2026-01-07 16:52:24 -0500

    updated home page copy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,18 @@ export default function Home() {
         <div className="home-content">
           <h1>Matt Boubin</h1>
           <div className="bio-text">
+            <h2>Senior Software Engineer</h2>
             <p>
-              I am a software engineer with 6 years of experience building data intensive systems.  
+              I am a software engineer with 7 years of experience building data intensive systems.  
               I currently work under NYU IT where I oversee the development and operation of the Engagement Data Platform at the NYU Wagner School of Public Service. 
             </p>
+            <h2>About Me</h2>
             <p>
               Senior Software Engineer with 7 years of experience designing, testing, and optimizing scalable software solutions who has become an expert in leading cross-functional teams, defining goals for robust projects, and implementing best software development practices to enhance performance and maintainability of backend services. 
               Currently seeking new opportunities to contribute projects that utilize distributed systems technologies.
             </p>
+            <h2>Skills</h2>
+            <h3>Languages</h3>
             <ul>
               <li><b>Languages</b></li>
               <li>Python (Flask, FastAPI, Django)</li>
@@ -21,7 +25,9 @@ export default function Home() {
               <li>Typescript</li>
               <li>SQL (PostgreSQL)</li>
               <li>MongoDB</li>
-              <li><b>Tools and Products</b></li>
+            </ul>
+            <h3>Technologies</h3>
+            <ul>
               <li>Cloud (GCP, AWS)</li>
               <li>CI/CD (Github Actions)</li>
               <li>Kubernetes</li>
